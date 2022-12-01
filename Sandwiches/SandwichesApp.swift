389ed0d30f8bad3a464d178bba8598a3d2244470
Artem Bolotov <teeemon@gmail.com>
2022-12-01 15:43:21 +0300

    Initial Commit

---
+++ Sandwiches/SandwichesApp.swift
@@ -0,0 +1,17 @@
+//
+//  SandwichesApp.swift
+//  Sandwiches
+//
+//  Created by artembolotov on 01.12.2022.
+//
+
+import SwiftUI
+
+@main
+struct SandwichesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
